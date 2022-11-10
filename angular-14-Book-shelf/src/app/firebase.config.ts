f156d2565270dc268a1421ce4a32ba6c3c93eabf
Shashank-Loading <99077787+Shashank-Loading@users.noreply.github.com>
2022-11-10 12:11:00 +0530

    book shelf app code

---
+++ angular-14-Book-shelf/src/app/firebase.config.ts
@@ -0,0 +1,11 @@
+// Import the functions you need from the SDKs you need
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyBm2ZPNOtd9xsT8RG90WigxUUzZYIzl2T8",
+  authDomain: "angular-14-894b2.firebaseapp.com",
+  projectId: "angular-14-894b2",
+  storageBucket: "angular-14-894b2.appspot.com",
+  messagingSenderId: "387448736244",
+  appId: "1:387448736244:web:3cc8625222fd1af7ea11a1"
+};
